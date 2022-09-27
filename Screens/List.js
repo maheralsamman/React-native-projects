9f0bb42dffddbed54cca6429c8e6746f75b3fd38
Maher Alsamman <56821558+maheralsamman@users.noreply.github.com>
2022-09-27 19:09:06 +0200

    Add files via upload

--- Screens/List.js
+++ Screens/List.js
@@ -3,12 +3,20 @@ import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
 import customers from "../customers";
 
 const List = ({ navigation }) => {
+    
+  const commentHandler = (userComment, userEmail) => {
+        customers.customers.map((customer)=> {
+          if(customer.email === userEmail) {
+              customer.comment = userComment;
+          }
+      }) 
+  }
 
   return (
     <View style={styles.container}>
-        {customers.customers.map((customer, index) => {
+        {customers.customers.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1).map((customer, index) => {
           return (
-            <Pressable key={index} onPress={() => navigation.navigate("Details", {customer})}>
+            <Pressable key={index} onPress={() => navigation.navigate("Details", {customer, commentHandler})}>
               <Text style={index % 2 ? styles.text1 : styles.text2}>{customer.name.first} {customer.name.last}</Text>
             </Pressable>
           );
@@ -18,7 +26,6 @@ const List = ({ navigation }) => {
 };
 
 export default List;
-
 
 const styles = StyleSheet.create({
     container: {
@@ -30,7 +37,7 @@ const styles = StyleSheet.create({
         textAlign: "left",
         margin : "6px",
         fontSize: "20px",
-        paddingLeft: ".5em"
+        paddingLeft: ".5em",
     },
     text2: {
         paddingLeft: ".5em",
@@ -38,6 +45,6 @@ const styles = StyleSheet.create({
         color: "white",
         textAlign: "left",
         margin : "6px",
-        fontSize: "20px"
+        fontSize: "20px",
       },
   });
