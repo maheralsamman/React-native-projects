27cb86f9cc3c91365af07c3081fcc1b2baa3eeec
Maher Alsamman <56821558+maheralsamman@users.noreply.github.com>
2022-09-27 19:09:20 +0200

    Add files via upload

--- Screens/Details.js
+++ Screens/Details.js
@@ -1,26 +1,42 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
 
-
 const Details = ({ route }) => {
-  const { customer } = route.params;
+  let { customer } = route.params;
+  const commentHandler = route.params.commentHandler;
+
   const [userComment, setUserComment] = useState("");
+  const [comment, setComment] = useState("");
+  const userEmail = customer.email;
+  const changeHandler = () => {
+    commentHandler(userComment, userEmail);
+    setComment(userComment);
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
-        Name: {customer.name.title} {customer.name.first} {customer.name.last}
+        Name:{" "}
+        <b>
+          {customer.name.title} {customer.name.first} {customer.name.last}
+        </b>
       </Text>
       <Text style={styles.text}>Email: {customer.email}</Text>
       <Text style={styles.text}>Gender: {customer.gender}</Text>
-      <Text style={styles.text}>Phone nb: {customer.phone}</Text>
-      <Text style={styles.text}>Comment: {userComment ? userComment : ""}</Text>
+      <Text style={styles.text}>Phone number: {customer.phone}</Text>
+      <Text style={styles.lastText}>
+        Comment: {customer.comment ? customer.comment : comment || ""}
+      </Text>
       <TextInput
+        testID='inputComment'
+        style={styles.textInput}
         placeholder="Details here..."
         value={userComment}
         onChangeText={(t) => setUserComment(t)}
       />
-    
+      <Pressable style={styles.button} onPress={changeHandler} testID='pressButton'>
+        <Text style={styles.buttonText}>Add comment</Text>
+      </Pressable>
     </View>
   );
 };
@@ -34,5 +50,27 @@ const styles = StyleSheet.create({
   text: {
     textAlign: "left",
     fontSize: "20px",
+  },
+  lastText: {
+    textAlign: "left",
+    fontSize: "20px",
+    marginBottom: 20,
+  },
+  textInput: {
+    border: "solid 2px black",
+    borderRadius: 5,
+    marginBottom: 25,
+    fontSize: 20,
+    padding: 10,
+    color: "green",
+  },
+  button: {
+    backgroundColor: "darkgreen",
+    padding: 20,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "white",
+    textAlign: "center",
   },
 });
