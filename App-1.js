f88985c9ceccb94afbff4bebf67e16b0ea074d59
Maher Alsamman <56821558+maheralsamman@users.noreply.github.com>
2022-09-27 19:12:51 +0200

    Add files via upload

--- App-1.js
+++ App-1.js
@@ -1,20 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
-import { useEffect, useState } from "react";
+import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet, Text, View, TextInput, Pressable,
+} from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  const [firstNumber, setFirstNumber] = useState("");
-  const [secondNumber, setSecondNumber] = useState("");
-  const [result, setResult] = useState("");
-  const [operation, setOperation] = useState("");
+  const [firstNumber, setFirstNumber] = useState('');
+  const [secondNumber, setSecondNumber] = useState('');
+  const [result, setResult] = useState('');
+  const [operation, setOperation] = useState('');
 
   useEffect(() => {
-    if (operation === "+") setResult(+firstNumber + +secondNumber);
-    if (operation === "-") setResult(+firstNumber - +secondNumber);
-    if (operation === "*") setResult(+firstNumber * +secondNumber);
-    if (operation === "/") setResult(+firstNumber / +secondNumber);
-    setFirstNumber("");
-    setSecondNumber("");
+    if (operation === '+') setResult(+firstNumber + +secondNumber);
+    if (operation === '-') setResult(+firstNumber - +secondNumber);
+    if (operation === '*') setResult(+firstNumber * +secondNumber);
+    if (operation === '/') setResult(+firstNumber / +secondNumber);
+    setFirstNumber('');
+    setSecondNumber('');
   }, [operation]);
 
   return (
@@ -23,10 +25,9 @@ export default function App() {
         placeholder="The first number"
         style={styles.textInput}
         keyboardType="numeric"
-        onChangeText={(text) => setFirstNumber(text)}
+        onChangeText={text => setFirstNumber(text)}
         value={firstNumber}
-        maxLength={10} //setting limit of input
-      />
+        maxLength={10} />
 
       <Text>{operation}</Text>
 
@@ -34,39 +35,35 @@ export default function App() {
         placeholder="The second number"
         style={styles.textInput}
         keyboardType="numeric"
-        onChangeText={(text) => setSecondNumber(text)}
+        onChangeText={text => setSecondNumber(text)}
         value={secondNumber}
-        maxLength={10} //setting limit of input
-      />
+        maxLength={10} />
       <View style={styles.buttons}>
-        <Pressable style={styles.button} onPress={() => setOperation("+")}>
+        <Pressable style={styles.button} onPress={() => setOperation('+')}>
           <Text style={styles.text}>+</Text>
         </Pressable>
         <Pressable
           style={styles.button}
-          onPress={() => setOperation("-")}
-          title="-"
-        >
+          onPress={() => setOperation('-')}
+          title="-">
           <Text style={styles.text}>-</Text>
         </Pressable>
         <Pressable
           style={styles.button}
-          onPress={() => setOperation("*")}
-          title="*"
-        >
+          onPress={() => setOperation('*')}
+          title="*">
           <Text style={styles.text}>*</Text>
         </Pressable>
         <Pressable
           style={styles.button}
-          onPress={() => setOperation("/")}
-          title="/"
-        >
+          onPress={() => setOperation('/')}
+          title="/">
           <Text style={styles.text}>/</Text>
         </Pressable>
       </View>
       <StatusBar style="auto" />
       <Text>
-        {result && "Your result is: "}
+        {result && 'Your result is: '}
         {result}
       </Text>
     </View>
@@ -76,29 +73,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   buttons: {
-    display: "flex",
-    flexDirection: "row",
-    gap: "2em",
-    marginTop: "1rem",
+    display: 'flex',
+    flexDirection: 'row',
+    gap: '2em',
+    marginTop: '1rem',
   },
   button: {
-    padding: "2em",
+    padding: '2em',
     height: 40,
-    color: "red",
+    color: 'red',
     borderRadius: 4,
     elevation: 3,
     backgroundColor: 'black',
   },
-  text:{
+  text: {
     fontSize: 16,
     lineHeight: 21,
     fontWeight: 'bold',
     letterSpacing: 0.25,
     color: 'white',
-  }
+  },
 });
