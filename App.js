76dd8786d41cafa2531761f58c58b3e0cef6bd1f
Maher Alsamman <56821558+maheralsamman@users.noreply.github.com>
2022-09-27 19:08:05 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, View, TextInput,  } from "react-native";
+import {
+  StyleSheet, Text, View, TextInput,
+} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import List from "./Screens/List.js"
-import Details from "./Screens/Details.js"
+import List from './Screens/List.js';
+import Details from './Screens/Details.js';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -15,6 +16,5 @@ export default function App() {
         <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
-    );
+  );
 }
-
